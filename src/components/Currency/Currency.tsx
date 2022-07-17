cf45637fdc331e4f6dd2cce1153babdc59a73f4f
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2022-07-17 14:56:36 -0700

    add number formatter

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,36 +1,17 @@
-import { motion, useAnimationControls } from "framer-motion"
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import { StatsContext } from "../../providers/StatsProvider"
+import numeral from 'numeral'
 
 import styles from './Currency.module.css'
 
 export default function Currency() {
     const [stats, setStats] = useContext(StatsContext)
-    const animation = useAnimationControls()
-
-    const variants = {
-        jump: {
-            opacity: [0, 1, 0],
-            y: [0, -25, 0],
-            transition: { duration: .3 },
-        }
-    }
-
-    useEffect(() => {
-        animation.start('jump')
-    }, [stats.currency])
 
     return (
         <div className={styles.currencyWrapper}>
             <div>
-                CURRENCY: {stats.currency}
+                CURRENCY: {numeral(stats.currency).format('0.0a')}
             </div>
-            <motion.div
-                className={styles.bumpNumber}
-                animate={animation}
-                variants={variants}>
-                +{stats.earnSpeed}
-            </motion.div>
         </div>
     )
 }
--- src/components/MainAction/MainAction.tsx
+++ src/components/MainAction/MainAction.tsx
@@ -1,53 +1,34 @@
-import { motion, useAnimationFrame } from "framer-motion"
+import { useAnimationFrame } from "framer-motion"
 import { useContext, useState } from "react"
 import { StatsContext } from "../../providers/StatsProvider"
 import ActionAnimation from "../Animation/ActionAnimation"
 import styles from './MainAction.module.css'
 
 const TIME_PER_FRAME = 1 / 60
+const UPDATE_STEP = 10
 
 export default function MainAction() {
     const [stats, setStats] = useContext(StatsContext)
-    const [toggle, setToggle] = useState(false)
 
-    const onClick = () => {
-        return () => {
-            setToggle(!toggle)
-        }
-    }
-
-    let frame = 0
+    let updateTick = 0
     useAnimationFrame(() => {
-        if (toggle) {
-            if (frame >= 1) {
-                setStats({
-                    ...stats,
-                    currency: stats.currency + stats.earnSpeed
-                })
-                frame = 0
-            }
+        if (updateTick > TIME_PER_FRAME * UPDATE_STEP) {
+            updateTick = 0
 
-            frame += TIME_PER_FRAME
+            setStats({
+                ...stats,
+                currency: stats.currency + (stats.earnSpeed * TIME_PER_FRAME * UPDATE_STEP)
+            })
         }
+
+        updateTick += TIME_PER_FRAME
     })
 
     return (
         <div className={styles.mainActionWrapper}>
 
-            <ActionAnimation isActive={toggle} />
+            <ActionAnimation isActive={true} />
 
-            <motion.button
-                whileHover={{
-                    scale: 1.1,
-                    transition: { duration: .1 },
-                }}
-                whileTap={{
-                    scale: .95,
-                    transition: { duration: .1 }
-                }}
-                onClick={onClick()}>
-                {toggle ? 'PAUSE' : 'START'}
-            </motion.button>
         </div >
     )
 }
